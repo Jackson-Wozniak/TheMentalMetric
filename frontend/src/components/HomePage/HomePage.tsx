6f4c7250136f853b53dcfe331a1d4ffcaf8d2ce9
Jackson Wozniak <jjw30@comcast.net>
2025-08-27 16:48:42 -0400

    add small filler box at bottom of screen to act as margin

--- frontend/src/components/HomePage/HomePage.tsx
+++ frontend/src/components/HomePage/HomePage.tsx
@@ -56,7 +56,7 @@ const HomePage: React.FC = () => {
 
     return (
         <ContentContainer>
-            <Box width="100%" maxHeight="90vh" display="flex" justifyContent="center">
+            <Box width="100%" height="100%" display="flex" justifyContent="center">
                 <Box sx={{display: "flex", flexWrap: "wrap", justifyContent: "center", 
                     gap: "40px", width: "60%", marginTop: "35px"}}>
                     {Games.map((game: Game, index: number) => {
--- frontend/src/components/Shared/ContentContainer.tsx
+++ frontend/src/components/Shared/ContentContainer.tsx
@@ -9,10 +9,12 @@ const ContentContainer: React.FC<{
 
     return (
         <Box width="100%" height="90%" marginBottom="20px" boxSizing="content-box" 
-            sx={{backgroundColor: theme.palette.background.primary}} overflow="auto">
-            <Box height="100%" sx={{ paddingBottom: "20px" }}>
+            display="flex" flexDirection="column"
+            sx={{backgroundColor: theme.palette.background.primary}}>
+            <Box height="98%" overflow="auto">
                 {children}
             </Box>
+            <Box height="2%" width="100%"></Box>
         </Box>
     )
 }
